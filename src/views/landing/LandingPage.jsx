d7e9d065d483c541963f13bd7f9819bbb220e298
worashf <uppert83@gmail.com>
2024-06-13 22:16:48 +0300

    Add  landing page

--- src/views/landing/LandingPage.jsx
+++ src/views/landing/LandingPage.jsx
@@ -1,48 +1,97 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Box, Typography, List, ListItem, ListItemText } from "@mui/material";
 
 const LandingPage = () => {
   return (
     <div className="flex min-h-screen flex-col bg-gray-100">
-  
       {/* Main Content Section */}
-      <main className="flex flex-grow flex-col  py-5  px-4 sm:px-6 lg:px-8">
-        <div className="w-full max-w-md ">
+      <main className="flex flex-grow flex-col py-5 px-4 sm:px-6 lg:px-8">
+        <div className="w-full max-w-2xl mx-5">
           <div>
-            <h2 className="mt-6 text-center text-3xl font-extrabold text-blue-600">
+            <h2 className="mt-6 text-3xl font-extrabold text-blue-800">
               ወደ ቅሬታ ማቅረቢያ መግቢያ እንኳን ደህና መጡ
             </h2>
-            <p className="mt-2 text-center text-base text-gray-700">
+            <p className="mt-2 text-lg text-gray-700">
               በአገልግሎታችን ላይ ያለዎትን ማንኛውም ቅሬታ የሚከተለው ማስፈንጠሪያ በመጫን ወደ ቅሬታ ማቅረቢያ ቅጹ
               ይግቡ
             </p>
           </div>
-          <div className="flex justify-center">
-            <Link
-              to="/auth/compliance-form"
-              className="border-transparent flex w-full items-center justify-center rounded-md border bg-blue-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-blue-700 md:py-3 md:px-4 md:text-lg"
-            >
-              Go to Compliance Form
-            </Link>
-          </div>
-          <div className="flex justify-between">
+          <Box display="flex" justifyContent="center" p={2}>
+            <Box maxWidth="600px">
+              <Typography variant="h4" component="h4" align="center" gutterBottom>
+                ስለቅሬታ ምንነትና አቀራረብ ምን ያህል የውቃሉ?
+              </Typography>
+              <Typography variant="body1" paragraph>
+                ቅሬታ ማለት የትኛውንም አይነት አገልግሎት እንዲሰጥ በህግ በተቋቋመ የመንግስት መ/ቤት ላይ ማንኛውም ተገልጋይ የሚያቀርበው በአገልግሎት ያለመርካት መግለጫ ሲሆን ስህተቶች ስለመፈፀማቸው በተለያዩ መንገዶች የሚደርሱና ምላሽ የሚሹ ጥቆማዎችንና አስተያየቶችን ያጠቃልላል፡፡
+              </Typography>
+              <Typography variant="h6" component="h6">
+                ዓላማዎች
+              </Typography>
+              <List>
+                <ListItem>
+                  <ListItemText primary="1. በመንግስት ተቋማት ውስጥ መልካም አስተዳደርን ማስፈንና አስተዳደራዊ በደልን መከላከል፣" />
+                </ListItem>
+                <ListItem>
+                  <ListItemText primary=" 2. በአገልግሎት አሰጣጥ ሂደት ላይ በተገልጋዮች ለቀረቡ ቅሬታዎች አፋጣኝ ምላሽ መስጠት፣" />
+                </ListItem>
+                <ListItem>
+                  <ListItemText primary="3. የአገልግሎት አሰጣጥን ለማሻሻልና የተገልጋዮችን እርካታ ለመጠበቅ የሚያግዙ የመረጃ ምንጭ ሆኖ ማገልገልና ለተጠቃሚዎች አለመርካት መንስኤ የሚሆኑ ስህተቶችን ማረም፡፡" />
+                </ListItem>
+                <ListItem>
+                  <ListItemText primary="4. መልካም አስተዳደርን ከማስፈን አኳያ የሚያጋጥሙ ችግሮችና መንስኤዎችን በመለየት እንዲቃለሉ/እንዲወገዱ/ ማድረግ፡፡" />
+                </ListItem>
+              </List>
+              <Typography variant="h6" component="h6">
+                መርሆዎች
+              </Typography>
+              <hr/>
+              <List>
+                <ListItem>
+                  <ListItemText primary="1. ቅሬታ የማቅረብና የመደመጥ እድል መስጠት" />
+                </ListItem>
+                <ListItem>
+                  <ListItemText primary="2. የቅሬታ አቀራረብን ቀላል ማድረግ፣" />
+                </ListItem>
+                <ListItem>
+                  <ListItemText primary="3. ለቀረበው ቅሬታ ፈጣን ምላሽ መስጠት" />
+                </ListItem>
+                <ListItem>
+                  <ListItemText primary="4. የቅሬታ አቀራረብ ስርዓቱን ውጤታማ ማድረግ፣ሃቀኛ፣ገለልተኛ፣ሚዛናዋ ሆኖ መገኘት፣" />
+                </ListItem>
+              </List>
+              <Typography variant="h6" component="h6">
+                የቅሬታ አቀራረብ ስነ-ስርዓት
+              </Typography>
+              <hr/>
+              <Typography variant="body1" paragraph>
+              የልደታ ክፍለ ከተማ ፐብሊክ ሰርቪስና የሰው ሃብት ልማት ጽ/ቤት የቅሬታ ያለው ማንኛውም አካል፡-
+              </Typography>
+              <List>
+                <ListItem>
+                  <ListItemText primary="ከታች ያለውን ሊንክ በመጫን መመዝገብ እና   ቅረታውን መሙላት ይችላል።" />
+                </ListItem>
+              </List>
+            </Box>
+          </Box>
+          <div className="flex justify-between mt-6">
             <div className="text-md text-gray-800">
               <p>
-              አለተመዘገቡም?{" "}
+                አለተመዘገቡም?{" "}
                 <Link
                   to="/auth/sign-up"
                   className="font-medium text-md text-blue-600 hover:text-blue-500"
                 >
-                    አካውንት ይፍጠሩ
+                  አካውንት ይፍጠሩ
                 </Link>
               </p>
               <p>
-              አካውንት አለዎት, እባክዎትን?{" "}
+                አካውንት አለዎት?{" "}
                 <Link
                   to="/auth/sign-in"
                   className="font-medium text-blue-600 hover:text-blue-500"
                 >
-                        ይግቡ
+                  ይግቡ
                 </Link>
               </p>
             </div>
